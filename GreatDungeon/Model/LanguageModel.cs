df859242b5e513f23ad7ec45739f81a306bacfdc
Dorota Przyjemska-Krusińska <ananeke@gmail.com>
2021-02-10 14:11:55 +0100

    I have to start with something

---
+++ GreatDungeon/Model/LanguageModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPFLocalizeExtension.Engine;
+using WPFLocalizeExtension.Extensions;
+
+namespace GreatDungeon.Model
+{
+    public static class LanguageModel
+    {
+        public static string GetLocalizedString(string key)
+        {
+            LocExtension localizeExtension = new LocExtension(key);
+            localizeExtension.ResolveLocalizedValue(out string uiString);
+            return uiString;
+        }
+        public static void SetLocale(CultureInfo culture = null)
+        {
+            if (culture != null)
+            {
+                LocalizeDictionary.Instance.Culture = culture;
+                CultureChange?.Invoke();
+            }
+        }
+        public static event Action CultureChange;
+    }
+}
---
+++ GreatDungeon/ViewModel/SettingsFlyoutViewModel.cs
@@ -0,0 +1,23 @@
+﻿using BaseClassesLibrary.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatDungeon.ViewModel
+{
+    public class SettingsFlyoutViewModel: BaseViewModel
+    {
+        private bool isOpen;
+        public bool IsOpen
+        {
+            get => isOpen;
+            set => SetProperty(ref isOpen, value);
+        }
+        public SettingsFlyoutViewModel()
+        {
+            IsOpen = false;
+        }
+    }
+}
